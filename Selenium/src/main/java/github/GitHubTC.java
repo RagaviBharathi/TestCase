06e5fdc6672dc769a91c74fdf29f029cfe17fdde
Ramesh T R <Ramesh T R@Sweethome>
2018-08-04 18:38:59 +0530

    Commented the error

--- Selenium/src/main/java/github/GitHubTC.java
+++ Selenium/src/main/java/github/GitHubTC.java
@@ -1,16 +1,16 @@
 package github;
 
-import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.chrome.ChromeDriver;
 
 public class GitHubTC {
 	 		public static void main(String[] args) {
 			// TODO Auto-generated method stub
-	ChromeDriver driver = new ChromeDriver();
+	/*ChromeDriver driver = new ChromeDriver();
 	driver.manage().window().maximize();
 	driver.get("http://layout.jquery-dev.com/demos/iframes_many.html");
 	int d= driver.findElementsByTagName("iframe").size();
 	System.out.println("Number of frames in the application:"+d);
-	driver.close();
+	driver.close();*/
 		}
 
 }
